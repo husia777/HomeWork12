77e63265563ef903fc78e2908b16312f567fabd3
husia777 <naimovsarif411@gmail.com>
2022-09-23 10:00:31 +0500

    дз 12 дня

---
+++ class_Storage.py
@@ -0,0 +1,103 @@
+from abc import ABC
+
+
+class Storage(ABC):
+
+    def __int__(self, items, capacity):
+        self._items = items
+        self._capacity = capacity
+
+
+    def add(self, name, count):
+        if count < self._capacity:
+            if self._capacity - count > 0:
+                if name in self._items:
+                    self._items[name] += count
+                    self._capacity -= count
+                else:
+                    self._items[name] = count
+                    self._capacity -= count
+
+    def add_initial_values(self, name, count):
+        self._items[name] = count
+        self._capacity -= count
+
+
+    def remove(self, name, count):
+        if name in self._items:
+            if self._items[name] >= count:
+                if self._items[name] - count > 0:
+                    self._items[name] -= count
+                    return True
+                elif self._items[name] - count == 0:
+                    del self._items[name]
+                    return True
+            else:
+                return False
+        else:
+            return False
+
+    def get_free_space(self):
+        """
+        :return:вернуть количество свободных мест
+        """
+        return self._capacity
+
+
+
+    def get_items(self):
+        """
+        :return: возвращает содержание склада в словаре {товар: количество}
+        """
+        for i in self._items:
+            print(f'{i}  {self._items[i]}')
+
+    def get_unique_items_count(self):
+        """
+        :return: возвращает количество уникальных товаров.
+        """
+        return len(self._items)
+
+
+
+class Store(Storage):
+
+    def __init__(self,  items, capacity=100):
+        self._items = items
+        self._capacity = capacity
+        for k, v in self._items.items():
+            self.add_initial_values(k, v)
+
+    def add_store(self, name, count):
+        if self.get_free_space() > count:
+            super().add(name, count)
+            return True
+        else:
+            return False
+
+class Shop(Storage):
+    def __init__(self, items, capacity=20):
+        self._items = items
+        self._capacity = capacity
+        for k, v in self._items.items():
+            self.add_initial_values(k, v)
+
+    def add_shop(self, name, count):
+        if self.get_unique_items_count() < 5 and self.get_free_space() > count:
+            super().add(name, count)
+            return True
+        else:
+            return False
+
+
+class Request:
+    def __init__(self, from_where, to, amount, product):
+        self.from_where = from_where
+        self.to = to
+        self.amount = amount
+        self.product = product
+
+    def __repr__(self):
+        return f'Доставить {self.amount} {self.product} из {self.from_where} в {self.to}'
+
+
---
+++ main.py
@@ -0,0 +1,56 @@
+from  class_Storage import *
+
+def main():
+    stor_items = {'печенье': 2, 'кофе': 7, 'чай': 2, 'сахар': 2, 'картофель': 2}
+    shop_items = {'печенье': 2, 'кофе': 7 }
+    stor = Store(stor_items)
+    shop = Shop(shop_items)
+
+    from_where = input('От куда будет осуществляться доставка - склад или  магазин')
+    to = input('Куда доставить')
+    product = input('Какой продукт доставить')
+    amount = int(input('Введите количество доставляемого продукта'))
+    conclusion = Request(from_where, to, amount, product)
+    print(conclusion.__repr__())
+
+
+    if from_where == 'склад':
+        if stor.remove(product, amount):
+            stor.remove(product, amount)
+            if shop.add_shop(product, amount):
+                shop.add_shop(product, amount)
+                print(f'Курьер забирает {amount} {product} из {from_where}')
+                print(f'Курьер везет {amount} {product} со {from_where} в {to}')
+                print(f'Курьер доставил {amount} {product} в {to}')
+            else:
+                print('В магазине недостаточно места, попобуйте что то другое')
+        else:
+            print('Не хватает на складе, попробуйте заказать меньше')
+
+
+
+    elif from_where == 'магазин':
+        if shop.remove(product, amount):
+            shop.remove(product, amount)
+            if stor.add_store(product, amount):
+                stor.add_store(product, amount)
+                print(f'Курьер забирает {amount} {product} из {from_where}')
+                print(f'Курьер везет {amount} {product} со {from_where} в {to}')
+                print(f'Курьер доставил {amount} {product} в {to}')
+            else:
+                print('На складе недостаточно места, попобуйте что то другое')
+
+        else:
+            print('Не хватает в  магазине, попробуйте заказать меньше')
+
+
+
+    print(f'В складе хранится:')
+    stor.get_items()
+    print(f'В магазине хранится:')
+    shop.get_items()
+
+
+if __name__ == "__main__":
+    main()
+
